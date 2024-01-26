6f65ed88f085a34f233dd44fb8dade614b9c8f94
bezkagoit <142536080+bezkagoit@users.noreply.github.com>
2024-01-26 20:26:41 +0000

    some updates

--- src/main.js
+++ src/main.js
@@ -1,26 +1,30 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+// Описаний у документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const BASE_URL = 'https://pixabay.com/api/';
+const BASE_URL = 'https://pixabay.com/api';
 const API_KEY = '41971380-5e7df6cf95dc1cfc66e370c4e';
 
-const searchForm = document.querySelector('.search-form');
-const resultContainer = document.querySelector('.result-container');
-const loader = document.querySelector('.loader');
+const refs = {
+  form: document.getElementById('search-form'),
+  resultContainer: document.getElementById('result-container'),
+  loader: document.querySelector('.loader'),
+};
 
-searchForm.addEventListener('submit', handleSearch);
+refs.form.addEventListener('submit', handleSearch);
 
 function handleSearch(event) {
   event.preventDefault();
 
-  loader.classList.remove('is-hidden');
-
   const form = event.currentTarget;
   const picture = form.elements.picture.value;
 
-  getPicture(picture)
+  showLoader();
+
+  searchPicturesByParams(picture)
     .then(data => {
       const pictures = data.hits;
 
@@ -30,12 +34,12 @@ function handleSearch(event) {
           message:
             'Sorry, there are no images matching your search query. Please try again!',
         });
-      } else {
-        createMarkup(pictures);
       }
+
+      createMarkup(pictures);
     })
-    .catch(error => {
-      console.error(error);
+    .catch(err => {
+      console.log(err);
       iziToast.error({
         position: 'topRight',
         message: 'Failed to fetch images. Please try again later.',
@@ -43,30 +47,30 @@ function handleSearch(event) {
     })
     .finally(() => {
       form.reset();
-      loader.classList.add('is-hidden');
+      hideLoader();
     });
 }
 
-function getPicture(picture) {
+function searchPicturesByParams(picture) {
   const urlParams = new URLSearchParams({
     key: API_KEY,
     q: picture,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
+    per_page: 15,
   });
 
-  return fetch(`${BASE_URL}?${urlParams}`).then(res => {
-    if (!res.ok) {
-      throw new Error(res.statusText);
+  return fetch(`${BASE_URL}?${urlParams}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
     }
-    return res.json();
+    return response.json();
   });
 }
 
-const lightbox = new SimpleLightbox('.gallery a', {
+let lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
-  captionPosition: 'bottom',
   captionDelay: 250,
 });
 
@@ -81,22 +85,31 @@ function createMarkup(hits) {
         views,
         comments,
         downloads,
-      }) => {
-        return `
-        <li class="gallery-item">
-          <a href="${largeImageURL}">
-            <img class="gallery-image" src="${webformatURL}" alt="${tags}">
-          </a>
-          <ul class="info-list">
-            <li class="info-item">Likes: ${likes}</li>
-            <li class="info-item">Views: ${views}</li>
-            <li class="info-item">Comments: ${comments}</li>
-            <li class="info-item">Downloads: ${downloads}</li>
-          </ul>
-        </li>`;
-      }
+      }) => `<li class="gallery-item">
+            <a href="${largeImageURL}">
+  <img class="gallery-image" src="${webformatURL}" alt="${tags}" width="370" heigth="300"></a>
+  <ul class="info-list">
+    <li class="info-item">Likes: ${likes}</li>
+    <li class="info-item">Views: ${views}</li>
+    <li class="info-item">Comments: ${comments}</li>
+    <li class="info-item">Downloads: ${downloads}</li>
+  </ul>
+</li>`
     )
     .join('');
-  resultContainer.innerHTML = markUp;
+  refs.resultContainer.innerHTML = markUp;
   lightbox.refresh();
+}
+function showLoader() {
+  const loader = document.querySelector('.loader');
+  if (loader) {
+    loader.classList.add('visible');
+  }
+}
+
+function hideLoader() {
+  const loader = document.querySelector('.loader');
+  if (loader) {
+    loader.classList.remove('visible');
+  }
 }
